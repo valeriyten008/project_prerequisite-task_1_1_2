53cd1c4862876011de4bd28728c2d7fa174a5950
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-03-31 23:55:30 +0900

    test21

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,14 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
 public class Main {
         public static void main(String[] args) {
-            UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+            UserService userDao = new UserServiceImpl();
 
             userDao.createUsersTable();
 
